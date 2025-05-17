2484896b14f1463754009b787bc877182c605b71
akshay123-pixle <ajadhao9696@gmail.com>
2025-05-17 19:26:26 +0530

    aa

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-// import type { NextConfig } from "next";
-//  import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-//  if (process.env.NODE_ENV === 'development') {
-//    await setupDevPlatform();
-//  }
-// export default nextConfig;
-
-
-const nextConfig = {
-  output: 'export',
+import type { NextConfig } from "next";
+ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+const nextConfig: NextConfig = {
+  /* config options here */
 };
-module.exports = nextConfig;
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
+export default nextConfig;
+
+
+// const nextConfig = {
+//   output: 'export',
+// };
+// module.exports = nextConfig;
